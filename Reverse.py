8121dac421842523b4f855e014898e1fd9157c4d
SuvethaRajendran <131169560+SuvethaRajendran@users.noreply.github.com>
2023-04-19 21:10:49 +0530

    Created Reverse.py
    This code will reverse the given array elements.

---
+++ Reverse.py
@@ -0,0 +1,7 @@
+a=[10,20,30,40,50]
+print("Original array elements: ")
+for i in range(0, len(a)):
+  print(a[i])
+print("Reversed array element: ")
+for i in range(len(a)-1, -1, -1):
+  print(a[i])
